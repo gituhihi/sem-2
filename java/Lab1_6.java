09bb67c42ef7122a57a7bdc93a5126eae6f7690d
Bhimani Maulik <140812702+gituhihi@users.noreply.github.com>
2024-01-09 21:14:26 +0530

    Update Lab1_6.java

--- java/Lab1_6.java
+++ java/Lab1_6.java
@@ -1,11 +1,1 @@
-import java.util.*;
-public class Lab1_6  {
-	public static void main(String[] args) {
-		// int a = Integer.parseInt(args[0]);
-		// int b = Integer.parseInt(args[1]);
-		int a=12;
-		int b=8;
-		int c=a+b;
-		System.out.println(c);	
-	}
-}
+
