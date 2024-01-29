b5bfede4f836bc61a7394b290f6a2693b930f365
Bhimani Maulik <140812702+gituhihi@users.noreply.github.com>
2024-01-29 17:05:12 +0530

    Create Lab4_5.java

---
+++ java/lab-4/Lab4_5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class  Lab4_5{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+        int length = inputString.length();
+        if (length % 2 == 0) {
+            int secondHalfStartIndex = length / 2;
+            String secondHalf = inputString.substring(secondHalfStartIndex);
+            System.out.println("Second half of the string: " + secondHalf);
+        } else {
+            // If the length is odd, print the second half starting from the middle character
+            int middleIndex = length / 2;
+            String secondHalf = inputString.substring(middleIndex);
+            System.out.println("Second half of the string: " + secondHalf);
+        }
+
+        scanner.close();
+    }
+}
